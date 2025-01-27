0ed54a1dbf8babe8090d379aa446bb4cc73b51ee
demo12341 <arshpatidar31@gamil.com>
2025-01-27 14:42:54 +0530

    updated scriot.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 document.addEventListener('DOMContentLoaded',()=> { 
     //Used this so that first DOM should be loaded and then the event should be added.
-    const button = document.getElementById('btn');
+    const button = document.getElementsById('btn');
     button.addEventListener('click', ()=> {
         confirm("You Want to Submit this Form");
         

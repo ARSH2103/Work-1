32e00d49322687cf9ad7c8c46bc508d0d119a33e
demo12341 <arshpatidar31@gamil.com>
2025-01-27 14:07:43 +0530

    removed the media query

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-document.addEventListener('DOMContentLoaded',()=> { //Used this so that first DOM should be loaded and then the event should be added.
+// Adding the event which is to performed as soon the button element is pressed.
+
+
+document.addEventListener('DOMContentLoaded',()=> { 
+    //Used this so that first DOM should be loaded and then the event should be added.
     const button = document.getElementById('btn');
     button.addEventListener('click', ()=> {
         confirm("You Want to Submit this Form");
+        
     });
+    
 });
+
+

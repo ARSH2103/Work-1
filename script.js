bd1a82e902b16dcd76857a3f024868f81a70a4af
demo12341 <arshpatidar31@gamil.com>
2025-01-27 15:10:41 +0530

    Done

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 document.addEventListener('DOMContentLoaded',()=> { 
     //Used this so that first DOM should be loaded and then the event should be added.
-    const button = document.getElementsById('btn');
+    const button = document.getElementById('btn');
     button.addEventListener('click', ()=> {
         confirm("You Want to Submit this Form");
         
